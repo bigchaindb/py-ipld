244202986da97111e3f7de196611f5d2f5bc90cf
tim <tim@ascribe.io>
2016-06-07 16:39:24 +0200

    Add unmarshal function

--- ipld/cbor.py
+++ ipld/cbor.py
@@ -1,14 +1,16 @@
 import copy
 
-from cbor import Tag, dumps
+from cbor import Tag, dumps, loads
 
 
+# NOTE: jbenet plans to register tag 258:
+#       https://www.iana.org/assignments/cbor-tags/cbor-tags.xhtml
 LINK_TAG = 258
 LINK_SYMBOL = '/'
 
 
-def marshal(original):
-    cp_original = copy.deepcopy(original)
+def marshal(json_data):
+    cp_json_data = copy.deepcopy(json_data)
 
     def transform(di):
         for k, v in di.items():
@@ -16,6 +18,7 @@ def marshal(original):
                 di[k] = transform(v)
 
         if LINK_SYMBOL in di.keys():
+            # TODO: Support: https://github.com/jbenet/js-multiaddr
             link = di[LINK_SYMBOL]
             di.pop(LINK_SYMBOL, None)
             if len(di.keys()) > 1:
@@ -25,8 +28,23 @@ def marshal(original):
 
     # TODO: Currently, all keys are being sorted. It is yet to be
     #       determined if this is compatible with other implementations.
-    return dumps(transform(cp_original), sort_keys=True)
+    return dumps(transform(cp_json_data), sort_keys=True)
 
 
-def unmarshal(cbor_buffer, opts):
-    pass
+def unmarshal(cbor_data):
+    json_data = loads(cbor_data)
+
+    def transform(di):
+        # NOTE: https://github.com/ipfs/js-ipld/blob/master/src/cbor.js#L81
+        #       Am I right in assuming that this is not supported in IPLD
+        #       anymore, as links cannot have properties themselves?
+        for k, v in di.items():
+            # NOTE: Dear Python-experts: Is this safe?
+            if isinstance(v, Tag):
+                di[k] = {
+                    LINK_SYMBOL: v.value
+                }
+            elif isinstance(v, dict):
+                di[k] = transform(v)
+        return di
+    return transform(json_data)
--- tests/test_cbor.py
+++ tests/test_cbor.py
@@ -1,6 +1,6 @@
 from cbor import Tag, dumps
 
-from ipld.cbor import marshal, LINK_TAG
+from ipld.cbor import marshal, unmarshal, LINK_TAG
 
 
 def test_transform_dict_to_cbor():
@@ -17,7 +17,7 @@ def test_transform_dict_to_cbor():
     assert marshal(src) == dumps(expected, sort_keys=True)
 
 
-def test_link_is_string():
+def test_transform_dict_with_link_to_cbor():
     src = {
         'hello': 'world',
         'num': 1,
@@ -33,3 +33,105 @@ def test_link_is_string():
     }
 
     assert marshal(src) == dumps(expected, sort_keys=True)
+
+
+def test_transform_dict_with_nested_link_to_cbor():
+    src = {
+        'hello': 'world',
+        'num': 1,
+        'l1': {
+            '/': 'takemedowntotheparadisecity',
+        },
+        'secret': {
+            'l1': {
+                '/': 'Ihhh ein Sekret!',
+            },
+        },
+    }
+
+    expected = {
+        'num': 1,
+        'hello': 'world',
+        'l1': Tag(LINK_TAG, 'takemedowntotheparadisecity'),
+        'secret': {
+            'l1': Tag(LINK_TAG, 'Ihhh ein Sekret!')
+        },
+    }
+
+    assert marshal(src) == dumps(expected, sort_keys=True)
+
+
+def test_transformation_doesnt_mutate_input():
+    src = {
+        'l1': {
+            '/': 'takemedowntotheparadisecity',
+        },
+    }
+
+    expected = {
+        'l1': {
+            '/': 'takemedowntotheparadisecity',
+        },
+    }
+
+    marshal(src)
+
+    assert src == expected
+
+
+def test_transform_cbor_to_dict():
+    src = dumps({
+        'hello': 'world',
+        'num': 1,
+    })
+
+    expected = {
+        'num': 1,
+        'hello': 'world',
+    }
+
+    assert unmarshal(src) == expected
+
+
+def test_transform_cbor_with_link_to_dict():
+    src = dumps({
+        'num': 1,
+        'hello': 'world',
+        'l1': Tag(LINK_TAG, 'takemedowntotheparadisecity'),
+    })
+
+    expected = {
+        'hello': 'world',
+        'num': 1,
+        'l1': {
+            '/': 'takemedowntotheparadisecity',
+        },
+    }
+
+    assert unmarshal(src) == expected
+
+
+def test_transform_cbor_with_nested_link_to_dict():
+    src = dumps({
+        'num': 1,
+        'hello': 'world',
+        'l1': Tag(LINK_TAG, 'takemedowntotheparadisecity'),
+        'secret': {
+            'l1': Tag(LINK_TAG, 'Ihhh ein Sekret!')
+        },
+    })
+
+    expected = {
+        'hello': 'world',
+        'num': 1,
+        'l1': {
+            '/': 'takemedowntotheparadisecity',
+        },
+        'secret': {
+            'l1': {
+                '/': 'Ihhh ein Sekret!',
+            },
+        },
+    }
+
+    assert unmarshal(src) == expected
